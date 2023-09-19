66b1f3ea88da451286cfacfcc0bed5539e890115
BMilliet <brunomilliet89@gmail.com>
2023-09-19 06:21:34 -0300

    fix render lag

---
+++ Space_Invaders/Views/GameView.swift
@@ -0,0 +1,44 @@
+import Swift2D
+import SwiftUI
+
+struct GameView: View {
+
+    @ObservedObject private var viewModel = GameViewModel()
+
+    var body: some View {
+        VStack {
+
+            viewModel.board
+
+            HStack {
+
+                Button("<") {
+                    viewModel.move(.left, id: "tank")
+                }
+                .keyboardShortcut(KeyEquivalent.leftArrow, modifiers: [])
+                .padding()
+
+                Button(">") {
+                    viewModel.move(.right, id: "tank")
+                }
+                .keyboardShortcut(KeyEquivalent.rightArrow, modifiers: [])
+
+            }
+
+            Button("space") {
+                viewModel.shoot()
+            }
+            .keyboardShortcut(KeyEquivalent.space, modifiers: [])
+            .padding()
+        }
+
+        .onAppear {
+            viewModel.startGame()
+        }
+    }
+
+    func render() {
+
+    }
+}
+
